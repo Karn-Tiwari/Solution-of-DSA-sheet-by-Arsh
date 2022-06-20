701308ba158dd04daa320cda520bfa627e114301
Karn Tiwari <karntiwari5353@gmail.com>
2022-06-20 20:44:27 +0530

    adhwf

---
+++ DSA/array/Equal_point_in_array.cpp
@@ -0,0 +1,20 @@
+
+
+  long sumLeft = 0 ;
+       long sum =0;
+       
+       for(int i=0; i<n; i++){
+           
+           sum+=arr[i];
+           
+       }
+       
+       for(int i=0; i<n; i++){
+           
+           sumLeft = sumLeft + arr[i];
+           
+           if(sumLeft-arr[i]==sum-sumLeft) return i+1;
+       }
+       
+       return -1;
+   }
